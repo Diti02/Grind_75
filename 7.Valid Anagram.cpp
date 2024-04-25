5087d669e622799d8e44f883863b20ebf00237dd
Diti02 <ditibanerjee02@gmail.com>
2024-04-25 20:43:38 +0530

    Valid Anagram

---
+++ 7.Valid Anagram.cpp
@@ -0,0 +1,10 @@
+    //TC: O(nlogn)
+    //SC: O(1)
+    bool isAnagram(string s, string t) {
+        sort(s.begin(),s.end());
+        sort(t.begin(), t.end());
+        if(s==t){
+            return true;
+        }
+        return false;
+    }
