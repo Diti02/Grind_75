3062ae54758988d7f7964be6ef3810be63dee420
Diti02 <ditibanerjee02@gmail.com>
2024-04-26 09:31:59 +0530

    2 Sum

--- 1.2sum.cpp
+++ 1.2sum.cpp
@@ -1,10 +1,8 @@
-
-
-        vector<int> twoSum(vector<int>& nums, int target) {
-        //TC:
-        // unordered_map:O(N)->for searching worst case O(N), then TC O(N^2), normally it is O(1)
-        // map:O(NlogN)
-        // SC:O(N)
+    //TC:
+    // unordered_map:O(N)->for searching worst case O(N), then TC O(N^2), normally it is O(1)
+    // map:O(NlogN)
+    // SC:O(N)        
+    vector<int> twoSum(vector<int>& nums, int target) {  
         map<int,int> mp;
         for(int i=0;i<nums.size();i++){
             int req=target-nums[i];
