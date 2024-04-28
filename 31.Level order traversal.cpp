a63102a5854697e9dbf2a95dca45c13932d6c7d4
Diti02 <ditibanerjee02@gmail.com>
2024-04-28 17:58:11 +0530

    level order traversal

---
+++ 31.Level order traversal.cpp
@@ -0,0 +1,28 @@
+    //TC:O(N)
+    //SC:O(N)
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        if(root==NULL){
+            return ans;
+        }
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            int size=q.size();
+            vector<int> level;
+            for(int i=0;i<size;i++){
+                TreeNode* f=q.front();
+                level.push_back(f->val);
+                q.pop();
+                if(f->left){
+                    q.push(f->left);
+                }
+                if(f->right){
+                    q.push(f->right);
+                }
+            }
+            ans.push_back(level);
+            } 
+
+        return ans;         
+        }
